16c05cd7d1fe49977ed9edbca3f9f29c43d0bcb7
ravshanmahmudov <ravshanmahmudov882@gmail.com>
2024-05-28 01:14:10 +1400

    masalar

---
+++ js/index.js
@@ -0,0 +1,228 @@
+// 1chi masala
+// alert("1 dan n gacha chiqarish")
+// let n = +prompt("");
+// for( let i =1; i<=n; i++){
+// console.log(i);
+// }
+
+// 2chi masala
+// alert("n dan 1 gacha chiqarish")
+// let n = +prompt("");
+// for( let i =n; i>=1; i--){
+// console.log(i);
+// }
+
+// 3chi masala
+// alert("1 dan n gacha sonlar yigindisi")
+// let d = 0;
+// let n = +prompt("");
+// for( let i =1; i<=n; i++){
+//     d=d+i;
+// }
+// console.log(d);
+
+
+// 4chi masala
+// alert("1 dan n gacha juft sonlarni yigindisini topish")
+// let n = +prompt("");
+// let sum = 0;
+// for( let i =1; i<=n; i++){
+//    if (i%2==0 ){
+//     sum=sum+i
+// }
+// }
+// console.log(sum);
+
+// 5chi masala
+// alert("1 dan n gacha toq sonlarni yigindisini topish")
+// let n = +prompt("");
+// let sum = 0;
+// for( let i =1; i<=n; i++){
+//    if (i%2==1 ){
+//     sum=sum+i
+// }
+// }
+// console.log(sum);
+
+
+// 6chi masala
+
+// alert("sonlar boluvchilarini chiqarish")
+// let n = +prompt("");
+// let m = 0;
+// for( let i = 1; i <= n; i++){
+//    if (n % i == 0 ){
+//     m=m+i
+//     console.log(i);
+// }
+// }
+
+// 7chi masala
+
+// alert("sonlar boluvchilarini chiqarish")
+// let n = +prompt("");
+// let m = 0;
+// for( let i = 1; i <= n; i++){
+//    if (n % i == 0 ){
+//     m=m+i
+//     console.log(i);
+// }
+// }
+// console.log(m);
+
+// 8chi masala
+// alert("tub yoki tub emasligini topish")
+// let n = +prompt("");
+// let m = 0;
+// for( let i = 1; i <= n; i++){
+//    if (n % i == 0 ){
+//     m=m+i
+//     console.log(i);
+// }if (p==2){
+// console.log(m);
+// }
+// }
+
+
+// 1chi masala
+
+// alert("k dan n gacha n marta chiqarish")
+// let k = +prompt("")
+// let n = +prompt("");
+// for (let i =1; i <=n; i++){
+//     console.log(k);
+
+// }
+
+// 2 chi masala 
+
+// let = prompt('a dan b gacha bolgan sonni va ular orasidagi sonlarni ham ekranga chiqarish');
+
+// let a = +prompt('a ni kiriting a > 0');
+
+// let b = +prompt('b ni kiriting');
+
+// for(let i = a; i <= b; i++) {
+// console.log(i);
+// }
+
+
+
+//  3 chi masala 
+
+// let = prompt('a dan b gacha bolgan sonlarni kamayish tartibda a va b dan tashqari sonlarni ekranga chiqarish');
+
+// let a = +prompt('a ni kiriting a > 0');
+
+// let b = +prompt('b ni kiriting');
+
+// for(let i = a - 1; i > b; i--) {
+// console.log(i);
+// }
+
+//   4 chi masala 
+// let i = +prompt('nechi kg konfet olasz');
+// let sum = 10.000; 
+
+// for(let i = 1; i <= 10; i++) {
+//   jami = sum * i;
+// }console.log(i + sum + jami + "som"); 
+
+//  5 chi masala  
+
+// let i = +prompt('nechi kg konfet olasz');
+// let sum = 10.000; 
+
+// for(let i = 0.1; i <= 1; i++) {
+//   jami = sum * i;
+// }console.log(i + sum + jami + "som"); 
+
+//  6 chi masala 
+
+// let i = +prompt('nechi kg konfet olasz');
+// let sum = 10.000; 
+
+// for(let i = 1.2; i <= 2; i+=0.2) {
+//   jami = sum * i;
+// }console.log(i + sum + jami + "som"); 
+
+// 7 chi masala  
+
+// let  = prompt('a dan b gacha bolgan sonning yigindisi topamiz a < b');
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+
+// let sum = 0;
+
+// for (let i = a ; i <= b; i+=1) {
+//   sum = sum + i;
+// }console.log(sum);
+
+
+
+//   8 chi masala 
+
+// let  = prompt('a dan b gacha bolgan sonning yigindisi topamiz a < b');
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+
+// let sum = 1;
+
+// for (let i = a ; i <= b; i+=1) {
+//   sum = sum * i;
+// }console.log(sum);
+
+
+
+//  9 chi masala 
+
+// let  = prompt('a dan b gacha bolgan barcha butun sonlarning kvadratlarining yigindisini topamiz a < b');
+
+// let a = +prompt('a ni kiriting');
+// let b = +prompt('b ni kiriting');
+
+// let sum = 0;
+
+// for (let i = a ; i <= b; i+=1) {
+//   sum = sum + i**2;
+// }console.log(sum);
+
+
+
+//  10 chi masala 
+
+// let n = +prompt('n ni kiriting');
+// let S = 0; 
+// for (let i = 1; i <= n; i++) {
+//     S += 1 / i;
+// }console.log(S);
+
+
+ 
+// 11 chi masala  
+
+// let n = +prompt('n ni kiriting');
+
+// let S = 0; 
+
+// for (let i = n; i <= 2 * n; i++) {
+//     S += i * i; 
+// }console.log(S);
+
+
+
+//   12 chi masala 
+
+// let n = +prompt('n ni kiriting');
+// let S = 1; 
+// for (let i = 1; i <= n; i++) {
+//     S *= 1 + i / 10;
+// }console.log(S);
+
+
+
+
+
+
